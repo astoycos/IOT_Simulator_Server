da4047d10a13b6f2a7e4e71eef7f356923a5c0cd
astoycos <astoycos@redhat.com>
2020-01-28 18:24:46 -0500

    Add all files

---
+++ public/simulator.js
@@ -0,0 +1,4 @@
+function sim(){
+    document.getElementById("rand_temp").innerHTML = "Temperature reading in Celsius is: " + Math.floor(Math.random() * 101);
+    console.log("Random Temp Generated");
+}
